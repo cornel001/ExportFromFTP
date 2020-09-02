08537eae47ca97fdeb515589ae5f39c76ed87e47
cornel001 <cornel.g.ionescu@gmail.com>
2020-09-02 14:58:12 +0300

    Initial Setup

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using Serilog;
+
+namespace ExportFromFTP
+{
+    public class Program
+    {
+        public static IConfiguration Configuration {get;} = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional:false, reloadOnChange:true)
+            .AddJsonFile($"appsettings{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}.json", optional:true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static void Main(string[] args)
+        {
+            System.IO.Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Host built.");
+            logger.LogInformation("App is Windows Service: {IsWinService}", 
+                                WindowsServiceHelpers.IsWindowsService());
+            host.Run();
+
+            Log.CloseAndFlush();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services.AddHostedService<Worker>())
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .UseSerilog()
+                .UseWindowsService();
+    }
+}
