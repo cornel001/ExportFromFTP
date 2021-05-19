132885a0e034f7052e2bf302f3658522921508d8
cornel001 <cornel.g.ionescu@gmail.com>
2021-05-19 12:41:28 +0300

    ExportService save to File

--- source/Services/ExportService.cs
+++ source/Services/ExportService.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace ExportFromFTP
@@ -15,7 +17,19 @@ namespace ExportFromFTP
         
         public async Task<bool> Export(ICollection<byte> file)
         {
-            _logger.LogInformation("Exported {count} bytes", file.Count);
+            try
+            {
+                using var stream = File.Open("ExportService.txt",FileMode.Append,FileAccess.Write);
+                using var writer = new StreamWriter(stream);
+                writer.WriteLine($"Exported {file.Count} bytes");
+            }
+            catch (IOException e)
+            {
+                _logger.LogCritical(e, "Export Service Malfunction", e.Message);
+                throw;
+            }
+
+            //_logger.LogInformation("Exported {count} bytes", file.Count);
             return await Task.FromResult(true);
         }
     }
